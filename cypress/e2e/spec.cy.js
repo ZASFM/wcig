65f846efbcde594fc877f64e856e6790a64ad4bd
shafi <shafi.bahrami.2015@gmail.com>
2023-11-15 16:25:11 +0100

    testing f

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Fundamentals', () => {
+
+  it('shows text title at fundamentals', () => {
     cy.visit('/fundamentals');
     cy.get('[data_test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+  });
+
+  it.only('shows the first accordion text',()=>{
+     cy.visit('/fundamentals');
+     //cy.contains(/Your tests will exists in a describe block./i).should('not.be.visible');
+     cy.get(`[data-test="accordion-item-1"] div[role="button"]`).click()
+     cy.contains(/Your tests will exist in a describe block./i).should('be.visible');
+     cy.get(`[data-test="accordion-item-1"] div[role="button"]`).click();
+     cy.contains(/Your tests will exist in a describe block./i).should('not.be.visible');
   })
 })
+
+
